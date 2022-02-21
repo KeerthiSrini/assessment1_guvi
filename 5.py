34bf50d446673acdb23edfd23ef7d0540e9d49d9
KeerthiSrini <keerthisrisha14@gmail.com>
2022-02-21 15:47:21 +0530

    added linear and binary search code

--- 5.py
+++ 5.py
@@ -1,1 +1,22 @@
 #Python program to implement linear search
+
+length = ""
+def linear(List,length,request):
+    for i in range(0, length):
+        if (List[i] == request):
+            return i
+    return 0
+    
+
+List = []
+size = int(input("Enter the Size of the List "))
+for i in range(size):
+    numbers = int(input("Enter the Elements to add in List "))
+    List.append(numbers)
+    length = len(List)
+request = int(input("Enter the Element to Find "))
+output = linear(List,length,request)
+if(output == 0):
+    print("Element not Found")
+else:
+    print("Index of the Element is", output)
--- 6.py
+++ 6.py
@@ -1,1 +1,28 @@
 #Python program to implement binary search
+
+def binary(List, request):
+    low = 0
+    mid = 0
+    high = len(List) - 1 
+    while low <= high:
+        mid = high + low
+        if List[mid] < request:
+            low = mid + 1
+        elif List[mid] > request:
+            high = mid - 1
+        else:
+            return mid
+    return 0
+
+
+List = []
+size = int(input("Enter the Size of the List "))
+for i in range(size):
+    numbers = int(input("Enter the Elements to add in List "))
+    List.append(numbers)
+request = int(input("Enter the Element to Find "))
+output = binary(List, request)
+if(output == 0):
+    print("Element not Found")
+else:
+    print("Index of the Element is", output)
